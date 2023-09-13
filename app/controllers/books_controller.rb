35e151c8f9991853dcc1eaf5d6b60ec6912f802e
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-13 11:56:22 +0000

    応用課題2　デバック　bookers2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+before_action :ensure_correct_user, only: [:edit, :update]
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @find_book = Book.new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @books = Book.all
+    @user = @book.user
+    if @book.save
+      redirect_to book_path(@book)
+      flash[:notice] = 'Book create successfully'
+    else
+      @books = Book.all
+      flash[:notice] = 'Book not create successfully'
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book update successfully'
+      redirect_to book_path(@book)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+    flash[:notice] = "Book was successfully destroyed."
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
+  def ensure_correct_user
+    user = Book.find(params[:id]).user
+    unless user == current_user
+      redirect_to books_path
+    end
+  end
+  
+end
