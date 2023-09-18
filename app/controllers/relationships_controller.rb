d3ab3dd874140aaa3cb8dfe99b46fe18feabf896
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-18 12:13:17 +0000

    relationship_controllerの記述

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,25 @@
 class RelationshipsController < ApplicationController
+
+    def create
+      relationship = Relationship.new(follower_id: current_user, followed_id: params[:user_id])
+      relationship.save
+      flash[:notice] = 'User is follow successfully.'
+      redirect_to request.referer
+    end
+
+    def destroy
+      relationship = Relationship.find_by(follower_id: current_user, followed_id: params[:user_id])
+      relationship.destroy
+      flash[:alert] = "User was Unfollowed successfully. "
+      redirect_to request.referer
+    end
+    
+    def following
+      @following_users = User.find(params[:user_id]).following_users
+    end
+    
+    def followed
+      @followed_users = User.find(params[:user_id]).followed_users
+    end
+    
 end
