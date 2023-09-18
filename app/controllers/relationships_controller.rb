35e9f77942a2b2f3e153185d2cf2a173c57c5d01
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-18 13:42:22 +0000

    フォロー、フォロワー機能実装完了

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,14 @@
 class RelationshipsController < ApplicationController
 
     def create
-      relationship = Relationship.new(follower_id: current_user, followed_id: params[:user_id])
+      relationship = Relationship.new(follower_id: current_user.id, followed_id: params[:user_id])
       relationship.save
       flash[:notice] = 'User is follow successfully.'
       redirect_to request.referer
     end
 
     def destroy
-      relationship = Relationship.find_by(follower_id: current_user, followed_id: params[:user_id])
+      relationship = Relationship.find_by(follower_id: current_user.id, followed_id: params[:user_id])
       relationship.destroy
       flash[:alert] = "User was Unfollowed successfully. "
       redirect_to request.referer
