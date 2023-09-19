da2b8fad2b66d3278d7d3ba68d1e5dcc62c9f033
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-19 06:41:54 +0000

    searchルート、コントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'searches/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to => "homes#top"
@@ -14,7 +15,7 @@ Rails.application.routes.draw do
     get 'relationships/following' => 'relationships#following', as: 'following'
     get 'relationships/followed' => 'relationships#followed', as: 'followed'
   end
-  
+  get "searches/search" => "searches#search", as: "search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
