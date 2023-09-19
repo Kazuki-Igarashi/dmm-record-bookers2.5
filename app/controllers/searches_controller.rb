5fc267e5ad6ebe00a5c1b09b3f92ef2186b991d6
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-19 07:54:42 +0000

    searchコントローラーの設定記述

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,16 @@
 class SearchesController < ApplicationController
+  before_action :authenticate_user!
+  
   def search
+    @model = params[:model]
+    @word = params[:word]
+    @method = params[:method]
+  
+    if @model == 'User'
+      @records = User.search_for(@word, @method)
+    else
+      @records = Book.search_for(@word, @method)
+    end
   end
+  
 end
