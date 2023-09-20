17a8d7027430a7e5a81bac4976bcdd121f4fe912
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-20 11:32:02 +0000

    記述追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,21 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.user_id = current_user.id
+    @book_comment.book_id = @book.id
+    @book_comment.save
     flash[:notice] = 'Comment create successfully'
-    redirect_to request.referer
+    # 非同期通信の為コメントアウト
+    # redirect_to request.referer
   end
 
   def destroy
     @book_comment = BookComment.find(params[:id])
     @book_comment.destroy
-    redirect_to request.referer
+    # 非同期通信の為コメントアウト
+    # redirect_to request.referer
     flash[:alert] = "Book was successfully destroyed."
   end
 
