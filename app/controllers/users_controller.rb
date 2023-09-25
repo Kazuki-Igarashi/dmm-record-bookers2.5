5ab1f89901d93c53cae1f1a17b06b3668c7968d4
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-25 11:24:33 +0000

    book showの記述追加 kaminari,today,yesterday,this_week,last_week

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books.page(params[:page]).reverse_order
+    @books = @user.books
+    @kaminari_books = @books.page(params[:page])
     @book = Book.new
-    @today_book = @books.created_today
-    @yesterday_book = @books.created_yesterday
+    @today_book = @books.created_days_ago(0)
+    @yesterday_book = @books.created_days_ago(1)
     @this_week_book = @books.created_this_week
     @last_week_book = @books.created_last_week
   end
