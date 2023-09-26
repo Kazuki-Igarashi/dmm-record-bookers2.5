02e64b0a4a5b8df60fb577f7d90936c3e94d083d
Kazuki-Igarashi <vki.0504@gmail.com>
2023-09-26 09:14:47 +0000

    search記述追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,14 @@ Rails.application.routes.draw do
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
+    get "search", to: "books#search"
   end
   
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy, :following_users, :followed_users]
     get 'relationships/following' => 'relationships#following', as: 'following'
     get 'relationships/followed' => 'relationships#followed', as: 'followed'
+    get "search" => "users#search"
   end
   get "searches/search" => "searches#search", as: "search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
